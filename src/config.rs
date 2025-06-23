d499431cbb2e720ca1b46b717a5b320d052f116d
youhavenopower2025 <youhavenopower2025@outlook.com>
2025-06-23 20:36:47 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["156.251.24.74"];
+pub const RS_PUB_KEY: &str = "XnQRyLdeB3Vpve0zEqt4p5PDmnG+FdWdB1kBgTNCvgc=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
